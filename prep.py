1cb8ea5e3cc362b437dc34c9fe346994dea1ce13
symeonw <symeonwhite16@gmail.com>
2020-04-29 20:19:02 -0500

    Auto data prep

--- prep.py
+++ prep.py
@@ -12,3 +12,12 @@ def get_data():
         df[cat] = df[cat].astype("category")
     return df
 
+df = get_data()
+for col in df.columns:
+    if col == "Age":
+        df.loc[df.sample(frac=0.5).index, col] = pd.np.nan #Makes fake Nan's
+    if col == "Education":
+        df.loc[df.sample(frac=0.7).index, col] = pd.np.nan #Makes fake Nan's
+
+col_nan_pct = df.isin([' ',np.nan]).mean() #Calculates percent of Nans
+col_names = col_nan_pct[col_nan_pct >= .1].index # Gets name of columns with over 50% Nans
